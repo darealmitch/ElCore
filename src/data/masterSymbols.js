94686238dcbbdc9cb67fe70cdb1c01be06455389
darealmitch <nathan.assimba@icloud.com>
2026-05-23 16:51:55 +0200

    Ajout du mapping complet des symboles de Classe des Maîtres

---
+++ src/data/masterSymbols.js
@@ -0,0 +1,106 @@
+export const masterSymbols = {
+    elsword: {
+        name: "Solace",
+        nameFr: "Solace",
+        image: "/images/master/solace-logo.png",
+        alt: "Logo de Classe de Maître Solace",
+    },
+
+    raven: {
+        name: "Solace",
+        nameFr: "Solace",
+        image: "/images/master/solace-logo.png",
+        alt: "Logo de Classe de Maître Solace",
+    },
+
+    ain: {
+        name: "Hernia",
+        nameFr: "Harnie",
+        image: "/images/master/harnie-logo.png",
+        alt: "Logo de Classe de Maître Harnie",
+    },
+
+    ara: {
+        name: "Gaia",
+        nameFr: "Gajar",
+        image: "/images/master/gajar-logo.png",
+        alt: "Logo de Classe de Maître Gajar",
+    },
+
+    laby: {
+        name: "Gaia",
+        nameFr: "Gajar",
+        image: "/images/master/gajar-logo.png",
+        alt: "Logo de Classe de Maître Gajar",
+    },
+
+    elesis: {
+        name: "Rosso",
+        nameFr: "Rossou",
+        image: "/images/master/rossou-logo.png",
+        alt: "Logo de Classe de Maître Rossou",
+    },
+
+    luciel: {
+        name: "Rosso",
+        nameFr: "Rossou",
+        image: "/images/master/rossou-logo.png",
+        alt: "Logo de Classe de Maître Rossou",
+    },
+
+    rena: {
+        name: "Ventus",
+        nameFr: "Ventus",
+        image: "/images/master/ventus-logo.png",
+        alt: "Logo de Classe de Maître Ventus",
+    },
+
+    rose: {
+        name: "Ventus",
+        nameFr: "Ventus",
+        image: "/images/master/ventus-logo.png",
+        alt: "Logo de Classe de Maître Ventus",
+    },
+
+    aisha: {
+        name: "Denif",
+        nameFr: "Agaur",
+        image: "/images/master/agaur-logo.png",
+        alt: "Logo de Classe de Maître Agaur",
+    },
+
+    chung: {
+        name: "Denif",
+        nameFr: "Agaur",
+        image: "/images/master/agaur-logo.png",
+        alt: "Logo de Classe de Maître Agaur",
+    },
+
+    eve: {
+        name: "Adrian",
+        nameFr: "Adrian",
+        image: "/images/master/adrian-logo.png",
+        alt: "Logo de Classe de Maître Adrian",
+    },
+
+    add: {
+        name: "Adrian",
+        nameFr: "Adrian",
+        image: "/images/master/adrian-logo.png",
+        alt: "Logo de Classe de Maître Adrian",
+    },
+
+    noah: {
+        name: "Ebalon",
+        nameFr: "Ebalon",
+        image: "/images/master/ebalon-logo.png",
+        alt: "Logo de Classe de Maître Ebalon",
+    },
+
+    lithia: {
+        name: "Beryl",
+        nameFr: "Beryl",
+        image: "/images/master/beryl-logo.png",
+        alt: "Logo de Classe de Maître Beryl",
+    },
+};
