a8ae3b43a106e38769251aa0be2c9d9bdffc1b9f
darealmitch <nathan.assimba@icloud.com>
2026-05-25 15:40:43 +0200

    Ajout d’un correctif global de scroll au changement de route

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -2,15 +2,17 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import ScrollToTopButton from "../ui/ScrollToTopButton";
+import ScrollToTopOnRouteChange from "./ScrollToTopOnRouteChange";
 
 function MainLayout() {
     return (
-        <div className="main-container">
+        <>
+            <ScrollToTopOnRouteChange />
             <Header />
             <Outlet />
             <ScrollToTopButton />
             <Footer />
-        </div>
+        </>
     )
 }
 export default MainLayout;
